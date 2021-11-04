43ab55db054330fdf6a5066073bffb1b9c294516
José Pedro <pedroosd28@gmail.com>
2021-11-04 00:34:56 -0300

    build: adicionando heroku.yml para buildar docker no heroku

--- docker-purge.sh
+++ docker-purge.sh
@@ -1,2 +1,2 @@
 # derruba todos os containers e os delete junto aos volumes
-docker-compose down; docker rm -f $(docker ps -a -q); docker volume rm $(docker volume ls -q); 
+docker-compose down; docker rm -f $(docker ps -a -q); 
