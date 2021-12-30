053cb0c3d06ccc9c432c0e92153e5a8ef839b9d8
José Pedro <pedroosd28@gmail.com>
2021-12-30 00:15:01 -0300

    build: ajustando caminho dos requirements.txt

--- app/src/settings.py
+++ app/src/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 SITE_URL = config("SITE_URL", default='http://localhost:8000')
+FRONTEND_URL = config('FRONTEND_URL', default='http://localhost:3000')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default="uma-cahve-bem-segura")
