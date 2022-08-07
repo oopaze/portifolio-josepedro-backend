3475960b684b71eb0e81574e992c67b801629bd2
José Pedro <pedroosd28@gmail.com>
2022-08-07 00:25:04 -0300

    feat: adding create random user command

---
+++ app/user/management/commands/create_random_user.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+
+from ...models import User
+
+from uuid import uuid4
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+      user = User.objects.create(**{
+        "email": f"{uuid4()}@gmail.com",
+        "username": "",
+        "first_name": "User",
+        "last_name": "Random"
+      })
+      
+      password = str(uuid4())
+      user.set_password(password)
+      
+      print(user.email, "seu email")
+      print(password, "sua senha")
